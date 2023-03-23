07b67e491f885328e76c24c50e6275c70531e5f1
Grifo89 <calaca89@gmail.com>
2023-03-23 15:43:20 -0500

    Using next link

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,13 @@
 // The component can have any name, but you must export it as a `default` export.
+import Link from "next/link"
 
 export default function FirstPost() {
-    return <h1>First Post</h1>
+    return (
+        <>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    )
 }
