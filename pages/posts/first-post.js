dfd7ec0aa536e6cd362dfb0667235fd7ce648aa3
Grifo89 <calaca89@gmail.com>
2023-03-25 17:09:20 -0500

    Assets, Metadata and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,18 @@
 // The component can have any name, but you must export it as a `default` export.
 import Link from "next/link"
+import Head from "next/head"
+import Layout from "../../components/layout"
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
     )
 }
